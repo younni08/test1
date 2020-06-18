f872b39ea3f60eda265b73fb9e1a50adbb2e6a2f
unknown <younni08@gmail.com>
2020-06-18 22:22:02 +0900

    server updated

---
+++ app.js
@@ -0,0 +1,6 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(3000, () => console.log('server is running on port 3000'));
